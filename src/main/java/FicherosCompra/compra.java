7e39d972832abb90e1ce4840c1ebfa80c152622e
Raul Martinez <raulmartinezmora2@gmail.com>
2026-03-16 11:08:19 +0100

    Activiades compra

--- src/main/java/FicherosCompra/compra.java
+++ src/main/java/FicherosCompra/compra.java
@@ -1,24 +1,31 @@
 package FicherosCompra;
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class compra {
+    private static Map productos = new HashMap<>();
+    private static Set lista = new HashSet<>();
+    private static void genproducto(){
+        productos.put("AVENA",2.21);
+        productos.put("GARBANZO",2.39);
+        productos.put("TOMATE",1.59);
+        productos.put("JENGIBRE",3.13);
+        productos.put("QUINOA",4.50);
+        productos.put("GUISANTES", 1.60);
+
+    }
     public static void main(String[] args){
 
-        String path = "C:\\Users\\raulm\\OneDrive\\Documentos\\GitHub\\producto.txt";
+        String path = "C:\\Users\\raulm\\Documents\\GitHub\\producto.txt";
         Scanner sc = new Scanner(System.in);
         String opcion="";
         int cantidad;
-        double pavena=2.21;
-        double pgarbanzo=2.39;
-        double tomate=1.59;
-        double jengibre=3.13;
-        double quinoa=4.50;
-        double guisantes=1.60;
+
+
        try {
            PrintWriter pw = new PrintWriter(path);
-
+           pw.println("PRODUCTO PRECIO CANTIDAD SUBTOTAL");
            while (!opcion.equalsIgnoreCase("FIN")) {
                impproductos();
                try {
@@ -28,15 +35,16 @@ public class compra {
                       opcion="FIN";
                       break;
                    }
-                   pw.print(opcion+" ");
-                       System.out.println("Introduce la cantidad");
-                       cantidad=sc.nextInt();
-                       if (cantidad>0){
-                           pw.print(cantidad);
-                       }
-
-
-
+                   pw.print(opcion+"\t");
+                   opcion.toUpperCase();
+                   productos.get(opcion);
+                   pw.print(productos.get(opcion)+"\t");
+                   System.out.println("Introduce la cantidad");
+                   cantidad=sc.nextInt();
+                   if (cantidad>0){
+                       pw.print(cantidad+"\t");
+                   }
+                   pw.print(productos.get(opcion)*cantidad);
                    pw.println();
                    sc.nextLine();
                } catch (Exception e) {
@@ -45,16 +53,18 @@ public class compra {
                }
            }
            pw.close();
+
            System.out.println("Saliendo del programa");
        }catch (IOException e){
            System.out.println(e.getMessage());
        }
     }
     public static void impproductos(){
-        System.out.println("------------------------------------------------");
+        System.out.println("-------------PRODUCTOS DISPONIBLES--------------");
         System.out.println("avena|garbanzos|tomate|jengibre|quinoa|guisantes");
         System.out.println("2,21 |   2,39  | 1,59 |  3,13  | 4,50 |   1,60  ");
         System.out.println("------------------------------------------------");
     }
+
 }
 
