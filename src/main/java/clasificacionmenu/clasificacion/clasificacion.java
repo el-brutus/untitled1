1b950a1ab5234113f83166586d659222a200710a
Raul Martinez <raulmartinezmora2@gmail.com>
2026-03-24 19:38:05 +0100

    terminacion practica jugadores

--- src/main/java/clasificacionmenu/clasificacion/clasificacion.java
+++ src/main/java/clasificacionmenu/clasificacion/clasificacion.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 
 public class clasificacion {
     public static void main(String[] args){
-        String path="src/main/java/jugadores/jugadores.txt";
-        String pathCV="src/main/java/jugadores/jugadoresCV.txt";
-        String pathAl="src/main/java/jugadores/alojados.txt";
+        String path="src/main/java/clasificacionmenu/clasificacion/jugadores.txt";
+        String pathCV="src/main/java/clasificacionmenu/clasificacion/jugadoresCV.txt";
+        String pathAl="src/main/java/clasificacionmenu/clasificacion/alojados.txt";
         String pathex="src/main/java/clasificacionmenu/clasificacion/jugadoresexcel.txt";
         String pathcl="src/main/java/clasificacionmenu/clasificacion/clasificacion.txt";
         HashMap<String, String> jugadores= new HashMap<>();
@@ -18,18 +18,20 @@ public class clasificacion {
             PrintWriter pw = new PrintWriter(pathCV);
             FileReader fr = new FileReader(path);
             BufferedReader br = new BufferedReader(fr);
-            String cadena = br.readLine();
+            String cadena;
             PrintWriter pw2 = new PrintWriter(pathAl);
             FileReader fr2 = new FileReader(path);
             BufferedReader br2 = new BufferedReader(fr2);
-            String cadena2 = br2.readLine();
-            while ((cadena  = br.readLine())!= null){
-                String[] partes = cadena.split(";");
+            String cadena2;
+            FileReader fr3 = new FileReader(path);
+            BufferedReader br3= new BufferedReader(fr3);
+            String cadena3;
+            while ((cadena3  = br3.readLine())!= null){
+                String[] partes = cadena3.split(";");
                 if (partes.length>2){
                     jugadores.put(partes[0],partes[2]);
                 }
             }
-            br.close();
             int opcion;
             boolean salida=false;
             while (salida==false) {
@@ -49,7 +51,7 @@ public class clasificacion {
 
                             pw.println("Jugadores federados en la CV");
                             pw.println("RANK\t-- Nombre");
-                            while (cadena != null) {
+                            while ((cadena = br.readLine()) != null) {
                                 String partes[] = cadena.split(";");
                                 if (partes.length > 2) {
                                     String info = partes[partes.length - 1];
@@ -59,7 +61,6 @@ public class clasificacion {
                                         pw.println(rank + "\t-- " + nombre);
                                     }
                                 }
-                                cadena = br.readLine();
                             }
                             System.out.println("Jugadores guardados");
                             pw.close();
@@ -70,7 +71,7 @@ public class clasificacion {
 
                             pw2.println("Jugadores alojados en un hotel");
                             pw2.println("RANK\t-- Nombre");
-                            while (cadena2 != null) {
+                            while ((cadena2 = br2.readLine())!= null) {
                                 String partes[] = cadena2.split(";");
                                 if (partes.length > 2) {
                                     String info = partes[partes.length - 1];
@@ -80,21 +81,20 @@ public class clasificacion {
                                         pw2.println(rank + "\t-- " + nombre);
                                     }
                                 }
-                                cadena2 = br2.readLine();
                             }
                             pw2.close();
                             br2.close();
                             System.out.println("Jugadores guardados");
                             break;
                         case 3:
-                            FileReader fr3=new FileReader(pathex);
-                            BufferedReader br3=new BufferedReader(fr3);
-                            String cadena3;
+                            FileReader fr4=new FileReader(pathex);
+                            BufferedReader br4=new BufferedReader(fr4);
+                            String cadena4;
                             PrintWriter pw3=new PrintWriter(pathcl);
                             System.out.println("Guardando clasificacion en un fichero...");
                             int posicion=-1;
-                            while ((cadena3 = br3.readLine()) != null){
-                                String[] partes = cadena3.split("\t");
+                            while ((cadena4 = br4.readLine()) != null){
+                                String[] partes = cadena4.split("\t");
                                 if (partes.length>2){
                                     String rank = partes[1];
                                     String pts = partes[3];
@@ -110,7 +110,8 @@ public class clasificacion {
                                 }
                             }
                             System.out.println("✔ Fichero guardado ✔");
-                            pw.close();
+                            pw3.close();
+                            break;
                         case 0:
                             salida = true;
                             break;
